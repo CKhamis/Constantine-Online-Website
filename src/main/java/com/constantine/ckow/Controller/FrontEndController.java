edb0adcd430aded5c123a7a43a4937a008d87adc
Constantin Khamis <costikhamis@gmail.com>
2023-10-02 11:37:53 -0700

    Begun implementing Tree page adopted from costionline.com. Still needs to have assets imported and wording changes

---
+++ src/main/java/com/constantine/ckow/Controller/FrontEndController.java
@@ -0,0 +1,24 @@
+package com.constantine.ckow.Controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import java.security.Principal;
+
+@Controller
+public class FrontEndController {
+    private static final String VERSION = "0.0.1";
+
+    @ModelAttribute
+    public void addCommonAttributes(Model model) {
+        model.addAttribute("theme", "/Dark.css");
+    }
+
+
+    @RequestMapping("/Tree")
+    public String getNewAccount() {
+        return "Tree";
+    }
+}
